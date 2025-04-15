9f090746f8bd96712177c63edb36d30b7174b702
Amr maher <amrm80233@>
2025-04-15 21:27:47 +0200

    add state to search input

--- src/app/Components/SearchComponent.jsx
+++ src/app/Components/SearchComponent.jsx
@@ -1,12 +1,24 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function SearchComponent() {
+  const [SearchValue, setSearchValue] = useState("")
+  const SendData = () => {
+    
+  }
   return (
     <>
       <div className="flex w-full items-center justify-center gap-2 flex-col md:flex-row">
-        <Input type="text" placeholder="Ask me anything..." />
-        <Button className="cursor-pointer" size="lg">Submit Question</Button>
+        <Input
+          type="text"
+          placeholder="Ask me anything..."
+          onChange={(event) => {setSearchValue(event.target.value);}}
+        />
+        <Button className="cursor-pointer" size="lg" onClick={SendData}>
+          Submit Question
+        </Button>
       </div>
     </>
   );
